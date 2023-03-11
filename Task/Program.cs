7e7796eeef68b6dccf23a4fa87b1ac9257b39b96
Angelika <wunderange@rambler.ru>
2023-03-11 21:24:18 +0300

    add task houlder

---
+++ Task/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+// Написать программу, которая из имеющегося массива строк
+//  формирует массив из строк, длина которых меньше или равна 3 символа.
+Console.WriteLine("Введите несколько слов через запятую: ");
+string text = Console.ReadLine();
+string[] data = text.Split(", ");
+
+
+for(int i=0; i < data.Length; i++)
+{
+    Console.Write($"{data[i]}, ");
+}
+
+
+
